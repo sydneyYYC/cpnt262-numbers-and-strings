c81f39765118cbc6f6dfeb914db7bec65b9f1b25
Sydney Bruce <sydneybruce20@gmail.com>
2023-10-07 17:25:15 -0700

    added before values

---
+++ client.js
@@ -0,0 +1,9 @@
+// variables
+let num = 2
+let string = 'Hello'
+
+// insert num variable into html
+document.querySelector(".num").innerHTML = num;
+
+// insert string variable into html
+document.querySelector(".string").innerHTML = string;
